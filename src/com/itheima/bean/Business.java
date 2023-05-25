d40b258a0a2a87ecb326c7e9fc5883816c303fc4
liangcetao <ahahahay51@gmail.com>
2023-05-25 13:33:45 +0800

    add Business class

---
+++ src/com/itheima/bean/Business.java
@@ -0,0 +1,31 @@
+package com.itheima.bean;
+
+public class Business extends User{
+    private String shopName;
+    private String address;
+
+    public Business(){
+
+    }
+    public Business(String loginName, String userName, String password, char sex, String phone, double money, String shopName, String address) {
+        super(loginName, userName, password, sex, phone, money);
+        this.shopName = shopName;
+        this.address = address;
+    }
+
+    public String getShopName() {
+        return shopName;
+    }
+
+    public void setShopName(String shopName) {
+        this.shopName = shopName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
